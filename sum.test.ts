fdd426f284f949902f175bc157a43601bdfa2052
Jeroen <eikenrode02@gmail.com>
2022-09-10 07:46:14 +0000

    Setting up initial kata state

--- sum.test.ts
+++ sum.test.ts
@@ -1,10 +1,11 @@
 import sum from "./sum";
 
-
+// Example of a test
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
+// Example of a failing test
 // test('adds 1 + 2 to equal 4', () => {
 //   expect(sum(1, 2)).toBe(4);
 // });
--- wordle.ts
+++ wordle.ts
@@ -1,17 +1,4 @@
 export function score(secret: string, guess: string): number[] {
-  let secretWord = secret
-  let result: number [] = new Array(5);
-  for (let i = 0; i < guess.length; i++) {
-    if (secretWord[i] === guess[i]) {
-      result[i] = 2;
-      secretWord = secretWord.replace(secretWord.charAt(i), "#")
-    }
-    else if(secretWord.includes(guess[i])) {
-      result[i] = 1;
-    }
-    else {
-      result[i] = 0;
-    }
-  }
-  return result
+  // TODO: implement me
+  return []
 }
